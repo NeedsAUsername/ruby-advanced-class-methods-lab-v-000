b7e49ad9a589b84789f31484f0c0258292e66a60
Kevin Chi <kevinchi118@gmail.com>
2018-06-20 01:21:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,10 +47,10 @@ class Song
 
   def self.new_from_filename(file_name)
       return TypeError unless file_name.end_with?(".mp3")
-      new_song = file_name.split(/[-.]/)[0..-2]
+      new_song = file_name.split(/[-.]/)[0..-2].collect{|word| word.strip}
       new_song_name = new_song[1]
       self.create_by_name(new_song_name)
-      
+
   end
 
 
